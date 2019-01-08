b90213171a651b40a5d6496b1baee984287ff247
AudreyAltman <audreykaltman@gmail.com>
2019-01-08 11:32:59 -0500

    fix sitemap subfile url

--- src/main/scala/dpla/batch_process_dpla_index/processes/Sitemap.scala
+++ src/main/scala/dpla/batch_process_dpla_index/processes/Sitemap.scala
@@ -5,8 +5,10 @@ import java.time.format.DateTimeFormatter
 
 import dpla.batch_process_dpla_index.helpers.{LocalFileWriter, ManifestWriter, S3FileWriter}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.storage.StorageLevel
+
+import scala.xml.Elem
 
 object Sitemap extends S3FileWriter with LocalFileWriter with ManifestWriter {
 
@@ -17,22 +19,23 @@ object Sitemap extends S3FileWriter with LocalFileWriter with ManifestWriter {
     val dateTime: ZonedDateTime = LocalDateTime.now().atZone(ZoneOffset.UTC)
     val maxRows: Int = 50000
 
-    val docs = spark.read.parquet(inpath)
+    val docs: DataFrame = spark.read.parquet(inpath)
 
     val ids: RDD[String] = docs.select("doc.id").rdd.map{ row => row.getString(0) }
       .persist(StorageLevel.MEMORY_AND_DISK_SER)
 
     // force evaluation to pull RDD into memory
-    val id_count = ids.count
+    val id_count: Long = ids.count
 
     val subfiles: Iterator[String] = ids.toLocalIterator.grouped(maxRows).zipWithIndex.map { case (ids, seq) => {
 
-      val timestamp = dateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"))
+      val timestamp: String = dateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"))
+      val subfileBase = timestamp + "/all_item_urls_" + seq + ".xml"
+      val subfileName = if (s3write) subfileBase + ".gz" else subfileBase
 
-      val subfileName = timestamp + "/all_item_urls_" + seq + ".xml"
-      val subfile = buildSubfile(dateTime, ids)
+      val subfile: String = buildSubfile(dateTime, ids)
 
-      if (s3write) writeS3Gzip(outpath, subfileName + ".gz", subfile)
+      if (s3write) writeS3Gzip(outpath, subfileName, subfile)
       else writeLocal(outpath, subfileName, subfile)
 
       subfileName
@@ -43,8 +46,9 @@ object Sitemap extends S3FileWriter with LocalFileWriter with ManifestWriter {
     if (s3write) writeS3(outpath, "all_item_urls.xml", siteMap)
     else writeLocal(outpath, "all_item_urls.xml", siteMap)
 
-    val opts = Map("Source" -> inpath, "Sitemap URL prefix" -> sitemapUrlPrefix, "URL count" -> id_count.toString)
-    val manifest = buildManifest(opts, dateTime)
+    val opts: Map[String, String] =
+      Map("Source" -> inpath, "Sitemap URL prefix" -> sitemapUrlPrefix, "URL count" -> id_count.toString)
+    val manifest: String = buildManifest(opts, dateTime)
 
     if (s3write) writeS3(outpath, "_MANIFEST", manifest)
     else writeLocal(outpath, "_MANIFEST", manifest)
@@ -55,9 +59,9 @@ object Sitemap extends S3FileWriter with LocalFileWriter with ManifestWriter {
 
   def buildSubfile(dateTime: ZonedDateTime, ids: Iterable[String]): String = {
 
-    val timestamp = dateTime.format(DateTimeFormatter.ISO_INSTANT)
+    val timestamp: String = dateTime.format(DateTimeFormatter.ISO_INSTANT)
 
-    val urls = ids.map(
+    val urls: Iterable[Elem] = ids.map(
       id => {
         <url>
           <loc>{"https://dp.la/item/" + id}</loc>
@@ -67,7 +71,7 @@ object Sitemap extends S3FileWriter with LocalFileWriter with ManifestWriter {
       }
     )
 
-    val xml = <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    val xml: Elem = <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       {urls}
     </urlset>
 
@@ -76,14 +80,14 @@ object Sitemap extends S3FileWriter with LocalFileWriter with ManifestWriter {
 
   def buildIndex(baseUrl: String, subfiles: Iterator[String], dateTime: ZonedDateTime): String  = {
 
-    val timestamp = dateTime.format(DateTimeFormatter.ISO_INSTANT)
+    val timestamp: String = dateTime.format(DateTimeFormatter.ISO_INSTANT)
 
-    val sitemapElements = subfiles.map( subfile => {
+    val sitemapElements: Iterator[Elem] = subfiles.map( subfile => {
       val url: String = baseUrl.stripSuffix("/") + "/" + subfile
       <sitemap><loc>{url}</loc><lastmod>{timestamp}</lastmod></sitemap>
     })
 
-    val xmlData =
+    val xmlData: Elem =
       <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{sitemapElements}</sitemapindex>
 
     xmlData.toString
