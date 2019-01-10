9bbfb53767786d0774dc378581f5d67a9642aca5
AudreyAltman <audreykaltman@gmail.com>
2019-01-10 12:42:24 -0500

    add datasource

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.3.2",
   "org.apache.spark" %% "spark-sql" % "2.3.2",
-  "com.amazonaws" % "aws-java-sdk" % "1.7.4"
+  "com.amazonaws" % "aws-java-sdk" % "1.7.4",
+  "org.elasticsearch" % "elasticsearch-spark-20_2.11" % "6.4.2"
 )
---
+++ src/main/scala/dpla/datasource/DefaultSource.scala
@@ -0,0 +1,15 @@
+package dpla.datasource
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.sources._
+
+class DefaultSource extends RelationProvider {
+
+  override def createRelation(sqlContext: SQLContext,
+                              parameters: Map[String, String]) : DplaDataRelation = {
+
+    val queryParams: Option[String] = parameters.get("queryParams")
+
+    new DplaDataRelation(queryParams)(sqlContext)
+  }
+}
