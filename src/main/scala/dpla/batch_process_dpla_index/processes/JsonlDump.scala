c696920f0c691222cc16dcf8a7b184585310a15c
AudreyAltman <audreykaltman@gmail.com>
2019-01-11 17:25:17 -0500

    try fewer maxRows on jsonl

--- src/main/scala/dpla/batch_process_dpla_index/processes/JsonlDump.scala
+++ src/main/scala/dpla/batch_process_dpla_index/processes/JsonlDump.scala
@@ -14,7 +14,7 @@ import scala.util.parsing.json._
 
 object JsonlDump extends S3FileWriter with LocalFileWriter with ManifestWriter {
 
-  val maxRows: Int = 10000000
+  val maxRows: Int = 1000000
 
   def execute(spark: SparkSession, outpath: String, query: String): String = {
 
@@ -72,7 +72,7 @@ object JsonlDump extends S3FileWriter with LocalFileWriter with ManifestWriter {
     val numPartitions: Int = (count / maxRows.toFloat).ceil.toInt
 
     data
-      .coalesce(numPartitions)
+      .repartition(numPartitions) // TODO: use coalesce instead?
       .saveAsTextFile(outDir, classOf[GzipCodec])
 
     val opts: Map[String, String] = Map(
