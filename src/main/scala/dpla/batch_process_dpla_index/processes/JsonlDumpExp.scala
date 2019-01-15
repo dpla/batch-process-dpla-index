3e65a363a9a94812bb85ca7d325f91607808a05f
AudreyAltman <audreykaltman@gmail.com>
2019-01-15 17:04:26 -0500

    try reading from dpla-master-dataset and write providers only

--- src/main/scala/dpla/batch_process_dpla_index/processes/JsonlDumpExp.scala
+++ src/main/scala/dpla/batch_process_dpla_index/processes/JsonlDumpExp.scala
@@ -6,7 +6,7 @@ import java.time.{LocalDateTime, ZoneOffset, ZonedDateTime}
 import dpla.batch_process_dpla_index.helpers.{LocalFileWriter, ManifestWriter, S3FileWriter}
 import org.apache.hadoop.io.compress.GzipCodec
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.storage.StorageLevel
 import org.elasticsearch.spark._
 import org.json4s.jackson.JsonMethods
@@ -28,8 +28,86 @@ object JsonlDumpExp extends S3FileWriter with LocalFileWriter with ManifestWrite
     val month: String = dateTime.format(DateTimeFormatter.ofPattern("MM"))
     val outDirBase: String = outpath.stripSuffix("/") + "/" + year + "/" + month
 
-    val keys = getS3Keys(s3client.listObjects("dpla-master-dataset")).toList
-      .filter(_.contains("IndexRecord.jsonl"))
+    val bucket = "dpla-master-dataset"
+
+    val allFiles = getS3Keys(s3client.listObjects(bucket)).toList
+
+    val paths = for {
+      file <- allFiles
+      sections = file.split("/")
+      if sections.length > 3
+      if sections(1) == "jsonl"
+      if sections(2).endsWith(".jsonl")
+    } yield (sections(0), sections(2))
+
+    val directories = for {
+      group <- paths.groupBy(x => x._1)
+      last = group._2.max
+    } yield (group._1, "s3a://" + bucket + "/" + group._1 + "/jsonl/" + last._2 + "/")
+
+    import spark.implicits._
+
+    val recordSources: List[(String, RDD[String])] = directories.map(x => {
+      val directory = x._2
+      val provider = x._1
+      val records: DataFrame = spark.read.text(directory).persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+      val recordSource = records.map(
+        row => {
+          val j = JsonMethods.parse(row.getString(0))
+          val source = j \ "_source"
+          // There are fields in legacy data files that we either don't need in
+          // Ingestion 3, or that are forbidden by Elasticsearch 6:
+          val cleanSource = source.removeField {
+            case ("_id", _) => true
+            case ("_rev", _) => true
+            case ("ingestionSequence", _) => true
+            case _ => false
+          }
+          JsonMethods.compact(cleanSource) // "compact" String rendering
+        }
+      ).rdd
+
+      (provider, recordSource)
+    }).toList
+
+    recordSources.foreach(x => {
+      val provider = x._1
+      val data = x._2
+      val outDir = s"$outDirBase/$provider.jsonl"
+      export(data, outDir, dateTime)
+    })
+
+    outpath
+  }
+
+  def export(data: RDD[String], outDir: String, dateTime: ZonedDateTime): Unit = {
+
+    val s3write: Boolean = outDir.startsWith("s3")
+
+    val count: Long = data.count
+
+    //    val numPartitions: Int = (count / maxRows.toFloat).ceil.toInt
+
+    // use repartition, coalesce is too slow
+//    data
+      //      .repartition(numPartitions)
+//      .saveAsTextFile(outDir, classOf[GzipCodec])
+
+    data.saveAsTextFile(outDir, classOf[GzipCodec])
+
+    val opts: Map[String, String] = Map(
+      "Record count" -> count.toString,
+      //      "Max records per file" -> maxRows.toString,
+      "Data source" -> "DPLA ElasticSearch Index")
+
+    val manifest: String = buildManifest(opts, dateTime)
+
+    if (s3write) writeS3(outDir, "_MANIFEST", manifest)
+    else writeLocal(outDir, "_MANIFEST", manifest)
+  }
+
+
 
 //    val configs = Map(
 //      "spark.es.nodes" -> "search-prod1-es6.internal.dp.la",
@@ -58,5 +136,5 @@ object JsonlDumpExp extends S3FileWriter with LocalFileWriter with ManifestWrite
 //    docs.count
 //
 //    outpath
-  }
+//  }
 }
