474014128ac45a32e1ef68bf7b8afd68435dadfc
AudreyAltman <audreykaltman@gmail.com>
2020-07-02 13:13:23 -0400

    draft ready to test

---
+++ src/main/scala/dpla/batch_process_dpla_index/entries/NecropolisEntry.scala
@@ -0,0 +1,36 @@
+package dpla.batch_process_dpla_index.entries
+
+import dpla.batch_process_dpla_index.processes.Necropolis
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Main entry point for generating a parquet dump of the ghost records.
+  *
+  * Args
+  *   args(0) = outpath   Local or S3 path to the top-level directory destination.
+  *                       Month and year will be added to the auto-generated file paths.
+  *                       e.g. s3a://necropolis/
+  *
+  * A spark-submit invocation requires the following packages:
+  *   com.amazonaws:aws-java-sdk:1.7.4
+  *   org.apache.hadoop:hadoop-aws:2.7.6
+  *
+  *   Double-check build file for correct package versions
+  */
+
+object NecropolisEntry {
+
+  def main(args: Array[String]): Unit = {
+
+    val outpath = args(0)
+    val newRecordsPath = args(1)
+
+    val conf: SparkConf = new SparkConf().setAppName("Batch process DPLA index: Necropolis")
+    val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+
+    Necropolis.execute(spark, newRecordsPath, outpath)
+
+    spark.stop()
+  }
+}
---
+++ src/main/scala/dpla/batch_process_dpla_index/processes/Necropolis.scala
@@ -0,0 +1,67 @@
+package dpla.batch_process_dpla_index.processes
+
+import dpla.batch_process_dpla_index.helpers.{LocalFileWriter, ManifestWriter, S3FileHelper}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
+object Necropolis extends S3FileHelper with LocalFileWriter with ManifestWriter {
+
+  // Expects input date in the format "YYYY/MM"
+  def execute(spark: SparkSession, newDataPath: String, outpath: String): String = {
+
+    val date: String = newDataPath.stripSuffix("/all.parquet/").split("/").reverse.take(2).reverse.mkString("/")
+    val lastDate: String = getLastDate(date)
+
+    val oldDataPath: String = s"s3a://dpla-provider-export/$lastDate/all.parquet/"
+    val newTombsPath: String = outpath.stripSuffix("/") + "/" + date + "/tombstones.parquet/"
+    val oldTombsPath: String = outpath.stripSuffix("/") + "/" + lastDate + "/tombstones.parquet/"
+
+    val newData: DataFrame = spark.read.parquet(newDataPath).select("doc.id")
+
+    val newTombs: DataFrame = spark.read.parquet(oldDataPath)
+      .select(
+        col("doc.id"),
+        col("doc.provider.name").as("provider"),
+        col("doc.dataProvider"),
+        col("doc.intermediateProvider"),
+        col("doc.isShownAt"),
+        col("doc.sourceResource.title").as("title"),
+        col("doc.sourceResource.collection.title").as("collection"))
+      .join(newData, Seq("id"), "leftanti")
+
+    val oldTombs = spark.read.parquet(oldTombsPath)
+
+    val tombstones = oldTombs.union(newTombs)
+
+    tombstones.write.parquet(newTombsPath)
+
+    val count = spark.read.parquet(newTombsPath).count
+    System.out.println("Tombstone count: " + count)
+
+    val opts: Map[String, String] = Map(
+      "Ghost record count" -> count.toString,
+      "New data source" -> newDataPath,
+      "Old data source" -> oldDataPath,
+      "Old tombs path" -> oldTombsPath)
+
+    newTombsPath
+  }
+
+  // Expects input date in the format YYYY/MM
+  // Returns date in the format YYYY/MM
+  def getLastDate(date: String): String = {
+    val year = date.split("/")(0)
+    val month = date.split("/")(1)
+    if (month == "01") (year.toInt - 1).toString + "/12"
+    else year + (month.toInt - 1).toString
+  }
+
+  def writeManifest(opts: Map[String, String], outDir: String): Unit = {
+    val s3write: Boolean = outDir.startsWith("s3")
+
+    val manifest: String = buildManifest(opts)
+
+    if (s3write) writeS3(outDir, "_MANIFEST", manifest)
+    else writeLocal(outDir, "_MANIFEST", manifest)
+  }
+}
