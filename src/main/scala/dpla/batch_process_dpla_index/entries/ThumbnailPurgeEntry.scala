731ec2166bc5c7eed73a545bb3644a07952ab3a7
Scott Williams <scott@dp.la>
2021-08-24 11:51:47 -0600

    Add invocation example

--- src/main/scala/dpla/batch_process_dpla_index/entries/ThumbnailPurgeEntry.scala
+++ src/main/scala/dpla/batch_process_dpla_index/entries/ThumbnailPurgeEntry.scala
@@ -4,6 +4,11 @@ import dpla.batch_process_dpla_index.processes.ThumbnailPurge
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
+/**
+  * Run as
+  * 
+  * sbt "runMain dpla.batch_process_dpla_index.entries.ThumbnailPurgeEntry ./data/20210706_151435-virginias-MAP4_0.MAPRecord.avro/"
+  */
 object ThumbnailPurgeEntry {
 
   def main(args: Array[String]): Unit = {
